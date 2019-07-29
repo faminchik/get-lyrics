d6e01f4568473ab6e076410df6ffd6fc61b40dc7
Faminchik Evgeniy <f.zhek@mail.ru>
2019-07-29 23:14:24 +0300

    rewriting: js → ts (Genuis class)

--- src/server/utils/Genius/Genius.ts
+++ src/server/utils/Genius/Genius.ts
@@ -1,14 +1,24 @@
 import _ from 'lodash';
 import config from 'config';
 import cheerio from 'cheerio';
+import { GenuisApiSearchResult, GeniusApiTrack } from 'ts/types/genius.types';
 import { fetchApiRequest, fetchHtmlRequest } from 'shared/utils/fetchRequests';
 import { findMostLikelyCorrectTrack } from 'server/utils/Genius/parseSearchResponse';
 
-const BASE_URL = config.get('genius-base-url');
-const SEARCH_PARAM = config.get('genius-param-search');
+const BASE_URL: string = config.get('genius-base-url');
+const SEARCH_PARAM: string = config.get('genius-param-search');
+
+type SearchOptions = {
+    path: string;
+    queryParams: { q: string };
+};
 
 export default class Genius {
-    constructor(accessToken) {
+    private accessToken: string;
+
+    private authHeader: { Authorization: string };
+
+    constructor(accessToken: string) {
         this.accessToken = accessToken;
 
         this.authHeader = {
@@ -16,49 +26,53 @@ export default class Genius {
         };
     }
 
-    _requestPromise(options) {
-        const { path, queryParams = {} } = options;
+    private requestPromise(options: SearchOptions): Promise<GenuisApiSearchResult | null> {
+        const { path, queryParams = { q: '' } } = options;
+        // TODO describe with types ↓
         return new Promise(resolve => {
             fetchApiRequest(BASE_URL, this.authHeader, path, queryParams).then(data => {
                 const status = _.get(data, 'meta.status');
-                if (status !== 200) {
-                    resolve(null);
-                }
+                if (status !== 200) resolve(null);
+
                 resolve(_.get(data, 'response', null));
             });
         });
     }
 
-    search(query) {
-        const options = {
+    private search(query: string): Promise<GenuisApiSearchResult | null> {
+        const options: SearchOptions = {
             path: SEARCH_PARAM,
             queryParams: {
                 q: query
             }
         };
 
-        return this._requestPromise(options);
+        return this.requestPromise(options);
     }
 
-    async getTrack(trackName) {
+    async getTrack(trackName: string): Promise<GeniusApiTrack | null> {
         const tracks = await this.search(trackName);
+        if (_.isNil(tracks)) return null;
+
         return findMostLikelyCorrectTrack(tracks, trackName);
     }
 
-    async getLyricsByTrackName(trackName) {
+    async getLyricsByTrackName(trackName: string): Promise<string> {
         const track = await this.getTrack(trackName);
+        if (!track) return '';
+
         const { url } = track;
-        const html = await fetchHtmlRequest(url);
+        const html: string = await fetchHtmlRequest(url);
 
-        return this._scrapLyrics(html);
+        return this.scrapLyrics(html);
     }
 
-    async getLyricsByTrackUrl(trackUrl) {
-        const html = await fetchHtmlRequest(trackUrl);
-        return this._scrapLyrics(html);
+    async getLyricsByTrackUrl(trackUrl: string): Promise<string> {
+        const html: string = await fetchHtmlRequest(trackUrl);
+        return this.scrapLyrics(html);
     }
 
-    _scrapLyrics(html) {
+    private scrapLyrics(html: string): string {
         const $ = cheerio.load(html);
 
         return $('.lyrics')
